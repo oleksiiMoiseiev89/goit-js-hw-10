e052915d49a2691329f9de3305f963d30ed860ec
oleksiiMoiseiev89 <alex.moiseiev.ua@gmail.com>
2024-10-27 19:52:16 +0100

    home work 10 js

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,36 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const delay = parseInt(form.delay.value, 10);
+  const state = form.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
